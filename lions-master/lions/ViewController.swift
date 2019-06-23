69b4171cf382b57f08cff248e62108647598c285
denis <zdi@bk.ru>
2019-06-23 02:45:05 -0700

    23/06/19

--- lions-master/lions/ViewController.swift
+++ lions-master/lions/ViewController.swift
@@ -1,24 +1,24 @@
 //
 //  ViewController.swift
-//  sights
+//  lions
 //
-//  Created by WSR on 6/23/19.
-//  Copyright © 2019 WSR. All rights reserved.
+//  Created by Администратор on 23/06/2019.
+//  Copyright © 2019 Sergey Klimovich. All rights reserved.
 //
 
 import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate  {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
-
+    
     let locationManager = CLLocationManager()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-   
+        
         mapView.delegate = self
         mapView.showsUserLocation = true
         
@@ -27,7 +27,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
             let location = mapView.userLocation
             location.title = "Я здесь"
         } else {
-            locationManager.requestAlwaysAuthorization()
+             locationManager.requestAlwaysAuthorization()
         }
         
         
@@ -35,7 +35,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
         // Do any additional setup after loading the view.
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         if CLLocationManager.locationServicesEnabled() {
             locationManager.delegate = self
@@ -44,7 +44,9 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         }
         
         let annotation = MKPointAnnotation()
-        annotation.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(55.00), longitude: 37.61)
+        annotation.title = "Object"
+        annotation.subtitle = "First Object"
+        annotation.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(55.37), longitude: 37.61)
         mapView.addAnnotation(annotation as! MKAnnotation)
     }
     
@@ -54,26 +56,25 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude), latitudinalMeters: radius, longitudinalMeters: radius)
         self.mapView.setRegion(region, animated: false)
     }
-    
-    
+
+
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         if annotation is MKUserLocation {
             return nil
         } else {
-            let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "annotationView")
-            let image = UIImage(named: "")
-            let size = CGSize(width: 20, height: 20)
+        let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "annotationView")
+        let image = UIImage(named: "s1200")
+            let size = CGSize(width: 50, height: 50)
             UIGraphicsBeginImageContext(size)
             image?.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
-            
+
             annotationView.image = UIGraphicsGetImageFromCurrentImageContext()
-            annotationView.canShowCallout = false
-            annotationView.isDraggable = true
-            
-            return annotationView
+        annotationView.canShowCallout = false
+        annotationView.isDraggable = true
+        
+        return annotationView
         }
     }
-    
-}
 
+}
 
